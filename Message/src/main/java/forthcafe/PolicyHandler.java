9871af40055c53173f972a0ce499af6039d3f42c
bigot93 <57469176+bigot93@users.noreply.github.com>
2021-03-04 10:10:20 +0900

    Add files via upload

--- Message/src/main/java/forthcafe/PolicyHandler.java
+++ Message/src/main/java/forthcafe/PolicyHandler.java
@@ -15,47 +15,5 @@ public class PolicyHandler{
 
     }
 
-    @StreamListener(KafkaProcessor.INPUT)
-    public void wheneverOrdered_(@Payload Ordered ordered){
-
-        if(ordered.isMe()){
-            System.out.println("##### listener  : " + ordered.toJson());
-        }
-    }
-    @StreamListener(KafkaProcessor.INPUT)
-    public void wheneverOrderCancelled_(@Payload OrderCancelled orderCancelled){
-
-        if(orderCancelled.isMe()){
-            System.out.println("##### listener  : " + orderCancelled.toJson());
-        }
-    }
-    @StreamListener(KafkaProcessor.INPUT)
-    public void wheneverPayed_(@Payload Payed payed){
-
-        if(payed.isMe()){
-            System.out.println("##### listener  : " + payed.toJson());
-        }
-    }
-    @StreamListener(KafkaProcessor.INPUT)
-    public void wheneverDeliveried_(@Payload Deliveried deliveried){
-
-        if(deliveried.isMe()){
-            System.out.println("##### listener  : " + deliveried.toJson());
-        }
-    }
-    @StreamListener(KafkaProcessor.INPUT)
-    public void wheneverDeliveryCancelled_(@Payload DeliveryCancelled deliveryCancelled){
-
-        if(deliveryCancelled.isMe()){
-            System.out.println("##### listener  : " + deliveryCancelled.toJson());
-        }
-    }
-    @StreamListener(KafkaProcessor.INPUT)
-    public void wheneverPayCancelled_(@Payload PayCancelled payCancelled){
-
-        if(payCancelled.isMe()){
-            System.out.println("##### listener  : " + payCancelled.toJson());
-        }
-    }
 
 }
